15b0214337a99fc0db410f74fc7c8eed292b46b8
JongHyunSeo <jonghyun9606@gmail.com>
2025-09-27 17:37:18 +0900

    Add Modules

--- app/src/main/java/com/example/hjstocker/MainActivity.kt
+++ app/src/main/java/com/example/hjstocker/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             HJStockerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hello",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
